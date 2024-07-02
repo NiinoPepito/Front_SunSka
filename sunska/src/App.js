59f414988483a6743169c1788e1e26cb69898aa8
NiinoPepito <clemdrb24120@gmail.com>
2024-07-02 17:34:33 +0200

    setup tailwind, router, archi

--- sunska/src/App.js
+++ sunska/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ConnexionPage from "./page/ConnexionPage";
+import StockBarPage from "./page/StockBarPage";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ConnexionPage />} />
+            <Route path="/stockbar" element={<StockBarPage />} />
         </Routes>
       </BrowserRouter>
   );
